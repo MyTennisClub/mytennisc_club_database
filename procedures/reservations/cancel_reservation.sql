6f830dece70fde72cd1f2b16fa65682e6d3babc9
giorgosbaknis <giorgosbaknis@gmail.com>
2024-06-01 21:07:48 +0300

    Cancel reservation

---
+++ procedures/reservations/cancel_reservation.sql
@@ -0,0 +1,16 @@
+DROP PROCEDURE IF EXISTS cancel_reservation;
+
+DELIMITER $$
+
+CREATE PROCEDURE cancel_reservation(
+    IN p_res_id BIGINT
+)
+BEGIN
+    UPDATE CourtReservations
+    SET res_status = 'CANCELLED'
+    WHERE res_id = p_res_id;
+END$$
+
+DELIMITER ;
+
+call cancel_reservation(1000000);
