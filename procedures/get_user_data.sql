cb032c8acb219bec175e346250b7e3d6c17046ef
giorgosbaknis <giorgosbaknis@gmail.com>
2024-06-01 16:18:27 +0300

    fixes

--- procedures/get_user_data.sql
+++ procedures/get_user_data.sql
@@ -6,11 +6,15 @@ CREATE PROCEDURE get_user_data_by_id(IN p_user_id INT)
 BEGIN
     SELECT
         CONCAT(user_first_name, ' ', user_last_name) AS full_name,
+        user_first_name,
+        user_last_name,
         user_birth_date,
         user_email,
         user_phone,
         user_address,
-        user_type
+        user_type,
+        user_has_children,
+        referred_by
     FROM
         SimpleUsers
     WHERE
