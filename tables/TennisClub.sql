d74f8c265f88587e06ef14fc5140594e8b265d03
Charilaos P. Stamelos <99473099+stamelosxp@users.noreply.github.com>
2024-06-02 08:07:25 +0300

    Upload Latest TennisClub.sql

---
+++ tables/TennisClub.sql
@@ -0,0 +1,16 @@
+drop table if exists TennisClub;
+CREATE TABLE if not exists TennisClub (
+    club_id INT AUTO_INCREMENT PRIMARY KEY,
+    club_name VARCHAR(255),
+    club_description TEXT,
+    club_address VARCHAR(255),
+    club_email VARCHAR(255),
+    club_website VARCHAR(255),
+    club_latitude DECIMAL(9,6) NOT NULL,
+    club_longitude DECIMAL(9,6) NOT NULL,
+    club_start_time time NOT NULL,
+    club_end_time time NOT NULL,
+    club_start_for_public time NOT NULL,
+    club_end_for_public time NOT NULL,
+    club_max_people_court  int not null DEFAULT 0
+);
